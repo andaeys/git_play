2d489b745d72b3dc27bebd812f54bc98bdc5e9ab
andaeys <ekayudha.sulistianda@quipper.com>
2022-01-21 22:48:10 +0700

    fix file not found

--- scripts/anda.sh
+++ scripts/anda.sh
@@ -15,8 +15,8 @@ git checkout -b $FLAG_KEY
 
 NEW_DATA="$FLAG_KEY=$FLAG_VALUE"
 
-FOUND_ROW="`grep -E ^"$FLAG_KEY\=*" ../flag/flags.txt\ `"
-if [ ! -z "$FOUND_ROW" -a "$FOUND_ROW" != " " ]; 
+FOUND_ROW="`grep -E ^"$FLAG_KEY\=*" ../flag/flags.txt`"
+if [ ! -z "$FOUND_ROW" -a "$FOUND_ROW" != " " ];
 then
         echo "matched $FOUND_ROW"
 else
@@ -24,10 +24,10 @@ else
         exit 1
 fi
 
-# grep -E ^"$FLAG_KEY\=*" ../flag/flags.txt\ 
+# grep -E ^"$FLAG_KEY\=*" ../flag/flags.txt\
 
 REPLACER="s/$FOUND_ROW/$NEW_DATA/"
-sed -i '' $REPLACER ../flag/flags.txt\ 
+sed -i '' $REPLACER ../flag/flags.txt\
 #cat ../flag/flags.txt\
 
 git add "../flag/flags.txt\ "
