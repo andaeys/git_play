42328d20bf20814e516006a4f2666f495d1f05a2
andaeys <ekayudha.sulistianda@quipper.com>
2022-01-21 22:34:17 +0700

    update script git commands

--- scripts/anda.sh
+++ scripts/anda.sh
@@ -1,7 +1,18 @@
+
+
 FLAG_FILE="../flag/flags.txt\ "
 
-FLAG_KEY="ENABLE_C"
-FLAG_VALUE="true"
+FLAG_KEY=$1
+FLAG_VALUE=$2
+#FLAG_KEY="ENABLE_C"
+#FLAG_VALUE="false"
+
+# git preparation
+git checkout master
+git pull origin master
+git checkout -b $FLAG_KEY
+# ===========================
+
 NEW_DATA="$FLAG_KEY=$FLAG_VALUE"
 
 FOUND_ROW="`grep -E ^"$FLAG_KEY\=*" ../flag/flags.txt\ `"
@@ -17,4 +28,12 @@ fi
 
 REPLACER="s/$FOUND_ROW/$NEW_DATA/"
 sed -i '' $REPLACER ../flag/flags.txt\ 
-cat ../flag/flags.txt\ 
+#cat ../flag/flags.txt\
+
+git add "../flag/flags.txt\ "
+git commit -m "$FLAG_KEY"
+git push -f origin $FLAG_KEY
+
+# requirment: gh
+which -a gh || abort "Install gh https://cli.github.com/manual/"
+gh pr create --title "$FLAG_KEY guys"
